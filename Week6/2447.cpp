a78159844bf40e87b8d4dc914015718ecec8fb04
currentnine <wjdgusrn951@naver.com>
2025-09-29 08:06:19 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 224 ms, Memory: 2020 KB

--- Week6/2447.cpp
+++ Week6/2447.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
-
-
 using namespace std;
 
+// (i, j) 좌표가 별인지 공백인지 판단해서 출력하는 재귀 함수
 void star(int i, int j, int n)
 {
+    // 현재 블록이 3x3 중간(공백 영역)에 해당하면 공백 출력
     if((i/n)%3==1 && (j/n)%3==1)
     {
         cout << " ";
@@ -13,11 +13,11 @@ void star(int i, int j, int n)
     {
         if(n==1) 
         {
-            cout << "*";
+            cout << "*"; // 더 이상 쪼갤 수 없는 크기면 별 출력
         }
         else 
         {
-            star(i,j,n/3);
+            star(i, j, n/3); // 더 작은 블록으로 나누어 재귀 호출
         }
     }
 }
@@ -27,16 +27,17 @@ int main()
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
+
     int n;
-    cin>>n;
-    for(int i =0;i<n;i++)
+    cin >> n; // 패턴 크기 입력 (3^k)
+
+    // n x n 전체 좌표 순회
+    for(int i = 0; i < n; i++)
     {
-        for(int j=0;j<n;j++)
+        for(int j = 0; j < n; j++)
         {
-            star(i,j,n);
+            star(i, j, n); // 각 좌표에 맞는 문자 출력
         }
-        cout<<"\n";
+        cout << "\n"; // 줄바꿈
     }
-
-
 }
